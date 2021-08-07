6c4cdb0fc509cb44059ac5ec9e8861a4ba42dd32
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-08 07:06:32 +0800

    feat: fruit species.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+const PORT = process.argv[2];
+
+// Set view engine
+app.set('view engine', 'ejs');
+
+app.get('/fruit', (request, response) => {
+  // Obtain data to inject into EJS template
+  const data = {
+    fruit: {
+      species: 'M. domestica',
+      name: 'banana',
+    },
+  };
+  // Return HTML to client, merging "index" template with supplied data.
+  response.render('fruit', data);
+});
+
+app.listen(PORT);
