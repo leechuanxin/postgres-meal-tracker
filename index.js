a2c12efe7aaefb347b036c7fe410de32cb0cac45
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-18 09:40:44 +0800

    feat: update meals.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import pg from 'pg';
 
 const { Client } = pg;
 
+// GLOBAL CONSTANTS
+const COLUMNS = ['type', 'description', 'amount_of_alcohol', 'was_hungry_before_eating'];
+
 // set the way we will connect to the server
 const pgConnectionConfigs = {
   user: 'chuanxin',
@@ -35,6 +38,11 @@ const whenQueryDone = (error, result) => {
   client.end();
 };
 
+const isInvalidAlcoholUnit = (unit) => (Number.isNaN(Number(unit)))
+ || (!Number.isInteger(Number(unit))) || (Number(unit) < 0);
+
+const isInvalidHungerValue = (unit) => unit.toLowerCase() !== 'true' && unit.toLowerCase() !== 'false';
+
 if (process.argv[2] === 'report') {
   const reportQuery = 'SELECT * FROM meals';
   client.query(reportQuery, whenQueryDone);
@@ -45,14 +53,14 @@ if (process.argv[2] === 'report') {
       'node index.js log <TYPE> <DESCRIPTION> <AMOUNT_OF_ALCOHOL> <WAS_HUNGRY_BEFORE_EATING>',
     );
     client.end();
-  } else if (Number.isNaN(Number(process.argv[5])) || !Number.isInteger(Number(process.argv[5]))) {
-    console.error('Error: The unit amount of alcohol consumed with your meal should be an integer.');
+  } else if (isInvalidAlcoholUnit(process.argv[5])) {
+    console.error('Error: The unit amount of alcohol consumed with your meal should be an integer of 0 or larger.');
     console.error('Your command has to be in the following format:');
     console.error(
       'node index.js log <TYPE> <DESCRIPTION> <AMOUNT_OF_ALCOHOL> <WAS_HUNGRY_BEFORE_EATING>',
     );
     client.end();
-  } else if (process.argv[6].toLowerCase() !== 'true' && process.argv[6].toLowerCase() !== 'false') {
+  } else if (isInvalidHungerValue(process.argv[6])) {
     console.error('Error: The fact that you are hungry before your meal can only be set to true or false.');
     console.error('Your command has to be in the following format:');
     console.error(
@@ -67,6 +75,42 @@ if (process.argv[2] === 'report') {
     ];
     const logQuery = 'INSERT INTO meals (type, description, amount_of_alcohol, was_hungry_before_eating) VALUES ($1, $2, $3, $4)';
     client.query(logQuery, input, whenQueryDone);
+  }
+} else if (process.argv[2] === 'edit') {
+  if (process.argv.length !== 6) {
+    console.error('Error: Your command has to be in the following format:');
+    console.error('node index.js edit <MEAL_ID> <COLUMN_NAME> <COLUMN_VALUE>');
+    client.end();
+  } else if (Number.isNaN(Number(process.argv[3])) || Number(process.argv[3]) < 1) {
+    console.error('Error: Please enter a valid MEAL_ID (1 or greater) to edit.');
+    console.error('The sample edit command is: node index.js edit <MEAL_ID> <COLUMN_NAME> <COLUMN_VALUE>');
+    client.end();
+  } else if (COLUMNS.indexOf(process.argv[4]) < 0) {
+    console.error('Error: Please enter a valid COLUMN_NAME to edit.');
+    console.error(`Valid COLUMN_NAMEs: ${COLUMNS.join(', ')}`);
+    console.error('The sample edit command is: node index.js edit <MEAL_ID> <COLUMN_NAME> <COLUMN_VALUE>');
+    client.end();
+  } else if (process.argv[4] === 'amount_of_alcohol' && isInvalidAlcoholUnit(process.argv[5])) {
+    console.error('Error: The unit amount of alcohol consumed with your meal should be an integer of 0 or larger.');
+    console.error('The sample edit command is: node index.js edit <MEAL_ID> <COLUMN_NAME> <COLUMN_VALUE>');
+    client.end();
+  } else if (process.argv[4] === 'was_hungry_before_eating' && isInvalidHungerValue(process.argv[5])) {
+    console.error('Error: The fact that you are hungry before your meal can only be set to true or false.');
+    console.error('The sample edit command is: node index.js edit <MEAL_ID> <COLUMN_NAME> <COLUMN_VALUE>');
+    client.end();
+  } else {
+    const colEdited = process.argv[4];
+    let editedValue = process.argv[5];
+    if (colEdited === 'amount_of_alcohol') {
+      editedValue = Number(editedValue);
+    } else if (colEdited === 'was_hungry_before_eating') {
+      editedValue = (process.argv[5].toLowerCase() === 'true');
+    } else {
+      editedValue = `'${editedValue}'`;
+    }
+
+    const editQuery = `UPDATE meals SET ${process.argv[4]} = ${editedValue} WHERE id=${process.argv[3]}`;
+    client.query(editQuery, whenQueryDone);
   }
 } else {
   console.error('Error: invalid command.');
