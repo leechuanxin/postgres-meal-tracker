3c459067c4a17be2dd4af613d44a6de55c4b1d6a
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-18 08:25:53 +0800

    feat: report query.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('hello world');
+import pg from 'pg';
+
+const { Client } = pg;
+
+// set the way we will connect to the server
+const pgConnectionConfigs = {
+  user: 'chuanxin',
+  host: 'localhost',
+  database: 'chuanxin',
+  port: 5432, // Postgres server always runs on this port
+};
+
+// create the var we'll use
+const client = new Client(pgConnectionConfigs);
+
+// make the connection to the server
+client.connect();
+
+// create the query done callback
+const whenQueryDone = (error, result) => {
+  // this error is anything that goes wrong with the query
+  if (error) {
+    console.log('error', error);
+  } else {
+    // rows key has the data
+    console.log(result.rows);
+  }
+
+  // close the connection
+  client.end();
+};
+
+if (process.argv[2] === 'report') {
+  const reportQuery = 'SELECT * FROM meals';
+  client.query(reportQuery, whenQueryDone);
+} else {
+  console.error('Error: invalid command.');
+  client.end();
+}
